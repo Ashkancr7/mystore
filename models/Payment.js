04d9853a0f87857a455325d1248ce3eb46cca6b1
unknown <ashkan.eng.asadi@gmail.com>
2025-06-06 17:45:07 +0330

    cha

--- models/Payment.js
+++ models/Payment.js
@@ -1,16 +1,13 @@
 const mongoose = require('mongoose');
 
-const paymentSchema = new mongoose.Schema(
-  {
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    amount: Number,
-    authority: String,
-    refId: String,
-    isPaid: Boolean,
-    orderId: { type: mongoose.Schema.Types.ObjectId, ref: 'Order' },
-    receiverName:String
-  },
-  { timestamps: true }
-);
+const paymentSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  amount: Number,
+  authority: String,
+  refId: String,
+  isPaid: { type: Boolean, default: false },
+  orderId: { type: mongoose.Schema.Types.ObjectId, ref: 'Order' },
+  receiverName: String
+}, { timestamps: true });
 
 module.exports = mongoose.model('Payment', paymentSchema);
